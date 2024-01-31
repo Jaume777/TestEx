71b25d8f73e44f8c1dbe5160bfb4bb24bba8df6c
Your Name <you@example.com>
2024-01-31 20:08:51 +0100

    Change by glen

--- TestEx/Program.cs
+++ TestEx/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, Montilivi!");
-            Console.WriteLine("I'm learning Git, woo!");
+            Console.WriteLine("I'm learning Git, changed by Glen");
         }
     }
 }
